55abcd3591b41de379f39d3e3ac38853a2de58b6
kyongh-Git <55642439+kyongh-Git@users.noreply.github.com>
2022-08-19 10:01:32 -0500

    Add files via upload

--- workpermitform.js
+++ workpermitform.js
@@ -6,6 +6,7 @@ var li1b= $("#element_1_2").val();
 var li2= $("#element_2").val();
 
 var li3a= $("#element_3_1").val();
+var li3b= $("#element_3_2").val();
 var li3c= $("#element_3_3").val();
 var li3d= $("#element_3_4").val();
 var li3e= $("#element_3_5").val();
@@ -94,7 +95,7 @@ if(offer=="1")
 var li54= $("#element_54").val();
  
 var querystring ="&element_1_1="+li1a+"&element_1_2="+li1b+"&element_2="+li2
-+"&element_3_1="+li3a+"&element_3_3="+li3c+"&element_3_4="+li3d+"&element_3_5="+li3e+"&element_3_6="+li3f
++"&element_3_1="+li3a+"&element_3_2="+li3b+"&element_3_3="+li3c+"&element_3_4="+li3d+"&element_3_5="+li3e+"&element_3_6="+li3f
 +"&element_20="+li4+"&element_21="+li5+"&element_6="+li6+"&element_7="+li7+"&element_8="+li8+"&element_22="+li9
 +"&element_42="+li10+"&element_11="+li11+"&element_12="+li12+"&element_13="+li13+"&element_35="+li35+"&element_38="+li21+"&element_40="+li51+"&element_41="+li54;
 querystring = querystring.replace(/\s/g,'%20');
